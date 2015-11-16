fe916360c4ec3b2c449d664879e6586c8ded65e9
Jenny Mangaly <jmangaly@gmail.com>
2015-11-16 16:50:17 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Looniversity::Application.config.secret_key_base = 'd64f1bfc25eacbeaaa0f14d4b6ec367eef22421152b131d7db0091c180b4086f12705098377be7bc25ad76c93b4503b05162fea45f77016d09fe1d0ef0a19aad'

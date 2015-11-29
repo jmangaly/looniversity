d578be7a4dfd8b06e1b58759de56e7516b9278d9
Jenny Mangaly <jmangaly@gmail.com>
2015-11-29 00:38:34 +0000

    Listing out the Courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Looniversity::Application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  resources :courses, :only => [:index, :show]
   namespace :instructor do
     resources :courses, :only => [:new, :create, :show]
   end
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -58,4 +60,4 @@ Looniversity::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+

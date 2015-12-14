206b2d1cc353ce15bc10c5c4d180e48de2409153
Jenny Mangaly <jmangaly@gmail.com>
2015-12-14 20:40:21 +0000

    Styling the Instructor Course Overview Page

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,7 +1,8 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course	
-  
+  before_action :require_authorization_for_current_course, :only => [:update]
+  before_action :require_authorization_for_current_section, :only => [:update]
+
   def new
     @section = Section.new
   end
@@ -11,17 +12,32 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end
 
+  def update
+    current_section.update_attributes(section_params)
+    render :text => 'Updated!'
+  end
+
   private
 
-  def require_authorized_for_current_course
-    if current_course.user != current_user
-      render :text => "Unauthorized", :status => :unauthorized
+  def require_authorization_for_current_section
+    if current_section.course.user != current_user
+      render :text => 'Unauthorization', :status => :unauthorization
     end
   end
 
-  helper_method :current_course
-  def current_course
-    @current_course ||= Course.find(params[:course_id])
+  def current_section
+    @current_section ||= Section.find(params[:id])
+  end
+
+  def require_authorization_for_current_course
+    if current_course.user != current_user
+      render :text => "Unauthorization", :status => :unauthorization
+    end
+  end
+
+ helper_method :current_course
+    def current_course
+    current_section.course
   end
 
   def section_params

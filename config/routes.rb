a28e4a88c32c009ba895e46855e5ad0343e61b4d
Jenny Mangaly <jmangaly@gmail.com>
2015-12-15 01:59:21 +0000

    Moving new lesson form into a modal

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Looniversity::Application.routes.draw do
   namespace :instructor do
     resources :lessons, :only => [:update]
     resources :sections, :only => [:update] do
-      resources :lessons, :only => [:new, :create]
+      resources :lessons, :only => [:create]
     end
 
     resources :courses, :only => [:new, :create, :show] do

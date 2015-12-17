9bab02f6d5db4c1bb9472cc33c8f212cae5334f3
Jenny Mangaly <jmangaly@gmail.com>
2015-12-17 02:12:57 +0000

    Changes to file

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Looniversity::Application.routes.draw do
       resources :lessons, :only => [:create]
     end
 
-    resources :courses, :only => [:new, :create, :show] do
+    resources :courses, :only => [:new, :create, :show, :update] do
       resources :sections, :only => [:create]
     end
   end
